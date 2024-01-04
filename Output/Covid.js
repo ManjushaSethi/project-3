847701824e375871611f8cf6a5ef7fc31fd8f92b
sorianor131 <sorianor131@gmail.com>
2024-01-03 19:06:11 -0500

    Added quickDBD exports

--- Output/Covid.js
+++ Output/Covid.js
@@ -31,148 +31,40 @@ dropdownMenu.append("option").attr("value",id).text(id);
 // First entry of the sample                 
 let first_entry = states_list[0];
 console.log(first_entry)
-})
-
-// // Make the base plots
-makeBar(first_entry);
-// makeBubble(first_entry);
-// makeDemographics(first_entry);
-// });
-// };
-
-
-
-// Make a Sample Bar PLot
-// function makeBar(sample){
-// // filter data to get id
-let sample_data =data.deaths_covid;
-// // apply a filter that matches based on sample id
-let results = sample_data.filter(sampled => sampled.id == sample);
-
-let trace1 ={
-                    x: states_list,
-                    y: data.deaths_covid,
-                    type:'bar'
-};
-Plotly.newPlot('bar', trace1);
+});
 }
 
+// Make the demographic info panel
+function panel(selectedValue) {
 
-// // First entry
-// let my_sample = results[0];
-// console.log(sample_data)
-// // Values of otu_ids, otu_labels, sample_values
-// let otu_ids = my_sample.otu_ids;
-// let otu_labels = my_sample.otu_labels;
-// let sample_values = my_sample.sample_values;
-// // Log the data to the console
-// console.log(otu_ids,otu_labels,sample_values);
-// // Slice the first 10 enteries
-//         let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-//         let xticks = sample_values.slice(0,10).reverse();
-//         let labels = otu_labels.slice(0,10).reverse();
-// // Establish Trace 1       
-//         let trace1 = {
-//                     x: xticks,
-//                     y: yticks,
-//                     text: labels,
-//                     name: "Samples",
-//                     type: "bar",
-//                     orientation: "h"
-//                   };
+    d3.json(url).then((data) => {
 
-// // Apply a title to the layout
-// let layout_1 = {
-//   title: "Top Ten OTUs",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-// }; 
-// Plotly.newPlot("bar", [trace1], layout_1);
-// });
-
-// };
-
-
-
-// function makeBubble(sample){
-// // use D3 to get data
-// d3.json(url).then((data) => {
-// // / filter data to get id
-// let sample_data =data.samples;
-// let results = sample_data.filter(sampled => sampled.id == sample);
-// // First result
-// let my_sample = results[0];
-
-// // Values of otu_ids, otu_labels, sample_values
-// let otu_ids = my_sample.otu_ids;
-// let otu_labels = my_sample.otu_labels;
-// let sample_values = my_sample.sample_values;            
+        // Create an array of the metadata objects
+        let metadata = data.metadata;
+        
+        // Filter data where id = selected value
+        let filteredData = metadata.filter((meta) => meta.id == selectedValue);
+      
+        // Assign the first object to obj variable
+        let obj = filteredData[0]
+        
+        // Use .html("") to clear any existing metadata
+        d3.select("#sample-metadata").html("");
   
-// // // Bubble Chart
-// let xticks = otu_ids.reverse();
-// let yticks = sample_values.reverse();
-// let text = otu_labels.reverse();
-// let marker = otu_ids;
-// // Establish Trace 2
-//  var trace2 = {
-//                     x: xticks,
-//                     y: yticks,
-//                     text:text,
-//                     mode: 'markers',
-//                      marker: {
-//                        color: otu_ids,
-//                       size: sample_values
-//                    }
-//                   };
-                  
-//       var layout = {
-//                     title: 'Bacteria Count',
-//                     xaxis: {title: "OTU ID"},
-//                     yaxis: {title: "Number of Bacteria"}
-                    
-//                   };
-                  
-//                   Plotly.newPlot('bubble', [trace2], layout);
-// });
+        // Use Object.entries() to return key-value pairs
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+        let results = Object.entries(obj);
 
-// };
+        // Iterate through the entries array
+        // Add a h5 child element for each key-value pair
+        results.forEach(([key,value]) => {
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
 
-
-
-
-// // Make Demographic window
-// function makeDemographics(sample){
-// d3.json(url).then((data) => {
-//  // filter data to get id
-// let sample_metadata =data.metadata;
-// console.log(sample_metadata);
-//                     // apply a filter that matches based on sample id
-//                     let results = sample_metadata.filter(sampled => sampled.id == sample);
-//                     // First entry
-//                     let my_sample_metadata = results[0];
-//                     console.log(my_sample_metadata);
-// //clear out previous entries in the demographic info 
-// d3.select('#sample-metadata').text('');
+        // Log the entries Array
+        console.log(results);
+    });
+  }
   
-// Object.entries(my_sample_metadata).forEach(([key, value]) => {
-//       console.log(key, value);
- 
-// d3.select('#sample-metadata').append('h3').text(`${key}, ${value}`); });
-
-
-// })};
-
-// // function to update dashboard with changing values
-// function optionChanged() {
-//   let newsample = d3.select("#selDataset").property("value");
-//   console.log(newsample)
-//   makeBar(newsample)
-//   makeBubble(newsample)
-//   makeDemographics(newsample)
-// };
 init()
 
---
+++ QuickDBD-export_CovidData.sql
@@ -0,0 +1,42 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Vaccine_DF" (
+    "_id" VAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "state" VAR   NOT NULL,
+    "total_vaccinations" INT   NOT NULL,
+    "total_distributed" INT   NOT NULL,
+    "people_vaccinated" INT   NOT NULL,
+    "total_vaccinations_per_hundred" INT   NOT NULL,
+    "people_vaccinated_per_hundred" INT   NOT NULL,
+    "distributed_per_hundred" INT   NOT NULL,
+    CONSTRAINT "pk_Vaccine_DF" PRIMARY KEY (
+        "state"
+     )
+);
+
+CREATE TABLE "Age_DF" (
+    "_id" VAR   NOT NULL,
+    "state" VAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "inpatient_beds_used_covid" INT   NOT NULL,
+    "total_adult_patients_hospitalized_confirmed_covid" INT   NOT NULL,
+    "total_pediatric_patients_hospitalized_confirmed_covid" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_20-29" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_30-39" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_40-49" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_50-59" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_60-69" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_70-79" INT   NOT NULL,
+    "previous_day_admission_adult_covid_confirmed_80+" INT   NOT NULL,
+    "deaths_covid" INT   NOT NULL,
+    CONSTRAINT "pk_Age_DF" PRIMARY KEY (
+        "state"
+     )
+);
+
+ALTER TABLE "Age_DF" ADD CONSTRAINT "fk_Age_DF_state_date" FOREIGN KEY("state", "date")
+REFERENCES "Vaccine_DF" ("state", "date");
+
